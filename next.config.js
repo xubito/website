6df124cdb3ce59fa380a0fab6f6acb3cf896c20b
Johannes Fritsch <fritsch@zoom7.de>
2023-07-30 18:15:05 +0200

    ...

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
   },
   reactStrictMode: true,
 }
